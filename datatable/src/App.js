f0d7ed78d63b030c3ad4aae21f736552ba5414b8
imimran <engr.aih@gmail.com>
2020-12-13 17:40:41 +0600

    display data from api

--- datatable/src/App.js
+++ datatable/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import { Container, Table, Form, FormControl, Button } from "react-bootstrap";
 
 function App() {
+   const [users, setUsers]  = useState("")
+
+
+useEffect(  () =>  {
+  const fetchData = async() =>{
+   const userData =  await axios.get("https://jsonplaceholder.typicode.com/users");
+   console.log(userData) 
+     setUsers(userData);
+  
+  }
+  fetchData()
+ 
+  
+}, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container className="my-5">
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+        <br></br>
+
+        <Table striped bordered hover variant="dark" mt-5>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th> Name</th>
+              <th> Email</th>
+              <th>Address</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {users.data &&
+              users.data.map((item) => (
+                <tr key={item.id}>
+                  <td>{item.id}</td>
+                  <td>{item.name}</td>
+                  <td>{item.email}</td>
+                  <td>{item.address.city}</td>
+                </tr>
+              ))}
+          </tbody>
+        </Table>
+      </Container>
     </div>
   );
 }
-
 export default App;
