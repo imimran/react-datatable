316f1ac90ff2ac790c031d130ea0cc01673d5e2b
imimran <engr.aih@gmail.com>
2020-12-14 14:53:13 +0600

    apply next prev

--- datatable/src/components/Pagination.js
+++ datatable/src/components/Pagination.js
@@ -1,6 +1,8 @@
 import React from "react";
+import  { useState } from "react";
 
 const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
+     const [currentPage, setCurrentPage] = useState(0)
   const pageNumbers = [];
 
   for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
@@ -8,8 +10,34 @@ const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
   }
 
   return (
-    <nav>
+    // <nav>
+    //   <ul className="pagination">
+    //     {pageNumbers.map((number) => (
+    //       <li key={number} className="page-item">
+    //         <a onClick={() => paginate(number)} href="!#" className="page-link">
+    //           {number}
+    //         </a>
+    //       </li>
+    //     ))}
+    //   </ul>
+    // </nav>
+
+    <nav aria-label="Page navigation example">
       <ul className="pagination">
+        {pageNumbers.includes(currentPage - 1) && (
+          <li className="page-item">
+            <a
+              className="page-link"
+              onClick={() => {
+                setCurrentPage(currentPage - 1);
+                paginate(currentPage - 1);
+              }}
+            >
+              Prev
+            </a>
+          </li>
+        )}
+
         {pageNumbers.map((number) => (
           <li key={number} className="page-item">
             <a onClick={() => paginate(number)} href="!#" className="page-link">
@@ -17,6 +45,20 @@ const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
             </a>
           </li>
         ))}
+
+        {pageNumbers.includes(currentPage + 1) && (
+          <li className="page-item">
+            <a
+              className="page-link"
+              onClick={() => {
+                setCurrentPage(currentPage + 1);
+                paginate(currentPage + 1);
+              }}
+            >
+              Next
+            </a>
+          </li>
+        )}
       </ul>
     </nav>
   );
